5b3e5c9304835cf48e7a604d9cfc1af3bb32f7d3
M-Yuki-Hasuo <huyuda1220@gmail.com>
2020-09-25 11:46:54 +0900

    add ConversationEvents

---
+++ Assets/ShowLog.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowLog : MonoBehaviour
+{
+    public PlayerController controller;
+
+    public void Run()
+    {
+        StartCoroutine(Message());
+    }
+
+    IEnumerator Message()
+    {
+        Debug.Log("やわ");
+        //会話イベントが発生するとプレイヤーは静止し、会話が終わるとまた動ける
+        //controller.controlEnabled = fasle;
+
+        //ラムダ式で書かないで!!Unity道場!!
+        yield return new WaitUntil(() => Input.GetButtonDown("Fire1"));
+        yield return null;
+
+        Debug.Log("ここには何もないよ");
+        yield return new WaitUntil(() => Input.GetButtonDown("Fire1"));
+        yield return null;
+
+        //controller.controlEnabled = true;
+    }
+}
