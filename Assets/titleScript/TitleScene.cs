be8bf0025f4d18ceb4351f30ee48772e8abf42b4
M-Yuki-Hasuo <huyuda1220@gmail.com>
2020-09-28 20:36:50 +0900

    add SceneManager.LoadScene

---
+++ Assets/titleScript/TitleScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleScene : MonoBehaviour
+{
+    //これまでpublicで変数宣言してきたが極力Findを用いてオブジェクト操作した方が好ましいのか？
+    GameObject startBtn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.startBtn = GameObject.Find("startButton");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void btn()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
