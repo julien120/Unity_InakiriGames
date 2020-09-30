59749aee6682f9a59b58f7ea40a548816f619961
M-Yuki-Hasuo <huyuda1220@gmail.com>
2020-09-30 15:22:07 +0900

    add userGuideScript

---
+++ Assets/StageSceneController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSceneController : MonoBehaviour
+{
+    public GameObject modalWindow;
+    bool swich = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void userGuide()
+    {
+        if (swich==false) {
+        this.modalWindow.SetActive(true);
+            swich = true;
+        }else if(swich == true)
+        {
+            this.modalWindow.SetActive(false);
+            swich = false;
+        }
+    }
+
+    public void stage1()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
