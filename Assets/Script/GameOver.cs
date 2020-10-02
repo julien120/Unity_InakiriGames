5a3ded011df1c9eb118476389b8595bcd0a5e64b
M-Yuki-Hasuo <huyuda1220@gmail.com>
2020-10-02 14:52:19 +0900

    add gameOverImage

--- Assets/Script/GameOver.cs
+++ Assets/Script/GameOver.cs
@@ -8,6 +8,7 @@ public class GameOver : MonoBehaviour
 {
     GameObject player;
     public static GameObject hpGauge;
+    public  GameObject canvass;
     //public GameObject hp;
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,7 @@ public class GameOver : MonoBehaviour
     public static void DecreaseHp()
     {
         hpGauge.GetComponent<Image>().fillAmount -= 0.25f;
+
     }
 
     // Update is called once per frame
@@ -26,15 +28,36 @@ public class GameOver : MonoBehaviour
         if(player.transform.position.y < -10.6f)
         {
             Debug.Log("gameOver");
+            canvass.SetActive(true);
             //コルーチンでゲームオーバー表示してから数秒後にとかの方がいいのかな？
-            SceneManager.LoadScene("Stagechoice");
+            StartCoroutine("sceneTransition");
+            //SceneManager.LoadScene("Stagechoice");
         }
     }
 
 
     public static void GameeOver()
     {
+        //canvass.SetActive(true);
+        SceneManager.LoadScene("Stagechoice");
+        //texttrue();
+    }
+
+    IEnumerator sceneTransition()
+    {
+        yield return new WaitForSeconds(5);
         SceneManager.LoadScene("Stagechoice");
     }
+
+    public void texttrue()
+    {
+        Debug.Log("gameOver");
+        canvass.SetActive(true);
+        //コルーチンでゲームオーバー表示してから数秒後にとかの方がいいのかな？
+        StartCoroutine("sceneTransition");
+        //SceneManager.LoadScene("Stagechoice");
+    }
+
+
 
 }
