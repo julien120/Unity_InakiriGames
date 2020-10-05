9fe9db83951f43fa67951f4784dffd35edddeeb7
M-Yuki-Hasuo <huyuda1220@gmail.com>
2020-10-05 23:28:06 +0900

    triggerは反応してるしCompoareTagの条件分岐もデバッグ取れてるジュリよ...

--- Assets/Script/ScoreUI.cs
+++ Assets/Script/ScoreUI.cs
@@ -13,7 +13,7 @@ public class ScoreUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(this);
+       DontDestroyOnLoad(this);
     }
 
     // Update is called once per frame
--- Assets/Script/fishMove.cs
+++ Assets/Script/fishMove.cs
@@ -8,37 +8,41 @@ public class fishMove : MonoBehaviour
     //public Transform goal;          //目的地となるオブジェクトのトランスフォーム格納用
     // private NavMeshAgent agent;     //エージェントとなるオブジェクトのNavMeshAgent格納用
 
-    public Vector3[] waypoint = new Vector3[4];
-    public GameObject[] waypointt = new GameObject[4];
-    private Rigidbody2D rb = null;
+   // public Vector3[] waypoint = new Vector3[4];
+    public GameObject[] waypoint = new GameObject[4];
+   // private Rigidbody2D rb = null;
     int count = 0;
+    GameObject target;
     //Public 型[] 変数名 = new 型名[5];
     // Start is called before the first frame update
     void Start()
     {
-        //エージェントのNaveMeshAgentを取得する
-        //agent = GetComponent<NavMeshAgent>();
-        //目的地となる座標を設定する
-        // agent.destination = goal.position;
-        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.Find("waypoint");
+        //rb = GetComponent<Rigidbody2D>();
         // rb.velocity = new Vector2(xSpeed, rb.velocity.y);
-        transform.position = Vector2.MoveTowards(transform.position, waypointt[0].transform.position, 5 * Time.deltaTime);
+       // transform.position = Vector2.MoveTowards(transform.position, waypointt[0].transform.position, 5 * Time.deltaTime);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //↓座標を変更してるから秒で移動する
-        //this.gameObject.transform.position = waypointt[0].transform.position;
-        if (this.gameObject.transform.position == waypointt[count].transform.position) {
-            count++;
-            transform.position = Vector2.MoveTowards(transform.position,waypointt[0].transform.position,5*Time.deltaTime);
-      
-        }
-        //tranlateでも瞬間移動ざむらい
-        //  this.gameObject.transform.Translate(this.waypointt[0].transform.position); //移動
-        //this.waypointt[0].transform.position *= 0.028f; //減速
+        //transform.position = Vector2.MoveTowards(transform.position, waypoint[count].transform.position, 10 * Time.deltaTime);
+        
 
+    }
+
+  
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        Debug.Log("判定");
+        if (coll.gameObject.CompareTag("waypointCollider"))
+        {
+           Debug.Log("wayポイント更新" + count);
+            transform.position = Vector2.MoveTowards(transform.position, waypoint[count].transform.position, 10 * Time.deltaTime);
+         
+            // count++;
+            Debug.Log("wayポイント更新" + count);
+        }
     }
 }
