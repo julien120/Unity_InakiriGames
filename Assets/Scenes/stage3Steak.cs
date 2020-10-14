9485198034ff27d28872d4c4b46ad136974fa595
M-Yuki-Hasuo <huyuda1220@gmail.com>
2020-10-14 11:59:51 +0900

    add stage3

---
+++ Assets/Scenes/stage3Steak.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class stage3Steak : MonoBehaviour
+{
+    public GameObject steak;
+    public GameObject UI;
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ScoreUI.steakCount += 300;
+      
+            Destroy(steak);
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
