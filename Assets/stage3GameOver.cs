a512da60d8316aad64f1704cad8e3c1e6256b418
M-Yuki-Hasuo <huyuda1220@gmail.com>
2020-10-14 18:17:31 +0900

    add stage3GameOverController

---
+++ Assets/stage3GameOver.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class stage3GameOver : MonoBehaviour
+{
+    GameObject player;
+    public GameObject GameOverCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.position.y == -28)
+        {
+            GameOverCanvas.SetActive(true);
+            StartCoroutine("Gameoover");
+        }
+    }
+
+    private IEnumerator Gameoover()
+    {
+        yield return new WaitForSeconds(3.0f);
+        SceneManager.LoadScene("StageChoice");
+    }
+   
+
+    }
